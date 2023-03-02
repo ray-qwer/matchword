6934a2f2a1bf2f2ad53042b66ec1a6d4a8c03a38
ray-qwer <b07901055@ntu.edu.tw>
2023-03-02 17:40:32 +0800

    textField value how to change

--- matchword_frontend/src/pages/CreateProb.js
+++ matchword_frontend/src/pages/CreateProb.js
@@ -5,25 +5,60 @@ import Typography from '@mui/material/Typography';
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import MenuItem from "@mui/material/MenuItem";
-import Select, { SelectChangeEvent } from "@mui/material/Select"
+import Select from "@mui/material/Select";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import { style, textAlign } from '@mui/system';
 
+const styleSheet = {
+    Paper:          {display:"flex", width:"100%", flexDirection:"column", paddingX:"30px", paddingY:"20px", margin:"20px"},
+    Container:      {paddingY:"24px", display:"flex", flexDirection:"column",flexGrow:1, alignItems:"center"},
+    GridContainer:  {display:"inherit", flexDirection:"row" ,justifyContent:"space-around",minWidth:"50%", marginY: "10px"},
+    GridText:       {display:"inherit",flexGrow:1, justifyContent:"center",alignItems:"center"},
+    GridInput:      {display:"inherit",flexGrow: 2},
+    Selector:       {width:"100%", justifyContent:"center",textAlign:"center"},
+    TextField:      {width:"100%"},
+    OptionText:     {justifyContent:"center"}
+    
+}
 function CreateProb(props){
+    const [size, setSize] = useState(4);
+    const [topic, setTopic] = useState("");
+    const handleChange = (event) => {
+        setSize(event.target.value);
+    }
 
     return(
         <Container sx={{display:"flex",flexGrow:1}}>
-            <Container sx={{paddingY:"24px", display:"flex", flexDirection:"column",flexGrow:1, alignItems:"center"}}>
-                <Paper elevation={3} sx={{display:"flex", width:"100%", flexDirection:"column", paddingX:"30px", paddingY:"20px"}}>
-                    <TextArea>
+            <Container sx={styleSheet.Container}>
+                <Paper elevation={3} sx={styleSheet.Paper}>
+                    <TextArea value="abc" onChange={setTopic}>
                         <Typography>題目:</Typography>
                     </TextArea>
                     <TextArea>
                         <Typography>敘述:</Typography>
                     </TextArea>
-                    <TextArea variant="select">
-                        <Typography>大小:</Typography>
-                    </TextArea>
+                    <Grid container sx={styleSheet.GridContainer}>
+                        <Grid item xs={2} sx={styleSheet.GridText}>
+                            <Typography>大小:</Typography>
+                        </Grid>
+                        <Grid item xs={8} sx={styleSheet.GridInput}>
+                            <Select
+                                sx={styleSheet.Selector}
+                                value={size}
+                                onChange={handleChange}
+                            >
+                                <MenuItem sx={styleSheet.OptionText} value={3}>3*3</MenuItem>
+                                <MenuItem sx={styleSheet.OptionText} value={4}>4*4</MenuItem>
+                                <MenuItem sx={styleSheet.OptionText} value={5}>5*5</MenuItem>
+                            </Select>
+                        </Grid>
+                    </Grid>
                 </Paper>
                 {/* another paper here */}
+                <Paper elevation={3} sx={styleSheet.Paper}>
+                    <Button>Press</Button>
+                </Paper>
             </Container>
         </Container>
     )
@@ -31,26 +66,17 @@ function CreateProb(props){
 
 function TextArea(props){
     const child = props.children
-    const variant = props.variant===undefined? "text": props.variant;
     return(
-        <Box sx={{display:"inherit", flexDirection:"row" ,justifyContent:"space-around",minWidth:"50%", marginY: "10px"}}>
-            <Box sx={{display:"inherit",flexGrow:1, justifyContent:"center",alignItems:"center"}}>
+        <Grid container sx={styleSheet.GridContainer}>
+            <Grid item xs={2} sx={styleSheet.GridText}>
                 {child}
-            </Box>
-            { variant === "text"? 
-                (<Box sx={{display:"inherit",flexGrow: 2}}>
-                    <TextField sx={{width:"100%"}}></TextField>
-                </Box>) 
-                : 
-                (<Box sx={{display:"inherit",flexGrow: 2}}>
-                    <Select
-                        
-                    >
-                        
-                    </Select>
-                </Box>)
-            }
-        </Box>
+            </Grid>
+            <Grid item xs={8} sx={styleSheet.GridInput}>
+                    <TextField sx={styleSheet.TextField} value={props.value} onChange={(e)=>{props.onChange(e.getValue);console.log(e)}}></TextField>
+            </Grid>
+        </Grid>
     )
 }
+
+
 export default CreateProb;
